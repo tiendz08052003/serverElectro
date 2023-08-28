4346bb9d68d41636442b31c38d4203b0086fb679
tiendz08052003 <nguyendinhtien0805@gmail.com>
2023-08-28 15:28:58 +0700

    update backend 6

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
-import("foo.json", { assert: { type: "json" } });
  function connect() {
-    mongoose.connect(`mongodb+srv://${config.username}:${config.password}@database.jxz0fmq.mongodb.net/${config.database}?retryWrites=true&w=majority`)
+    mongoose.connect(`mongodb+srv://nguyendinhtien08052003:tien12345@database.jxz0fmq.mongodb.net/WebSiteCongNghe?retryWrites=true&w=majority`)
     .then (() => {
         console.log("Connect successful!!!");
     })

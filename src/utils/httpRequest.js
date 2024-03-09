ba0deabfd238ffebff09b5397f458a9549105978
tiendz08052003 <nguyendinhtien0805@gmail.com>
2024-03-09 21:55:55 +0700

    update sever

--- src/utils/httpRequest.js
+++ src/utils/httpRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const httpRequest = axios.create({
-    baseURL: "https://be-electro-api.onrender.com/api/"
+    baseURL: "http://localhost:3001/api/"
 })
 
 export const get = async (path, options) => {

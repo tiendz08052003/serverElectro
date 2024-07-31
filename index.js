677402b8ce76db233994d23dc0619b9d6ba7fa13
tiendz08052003 <nguyendinhtien0805@gmail.com>
2024-07-31 08:30:38 +0700

    update server

--- index.js
+++ index.js
@@ -27,40 +27,41 @@ const io = new Server(server, {
         origin: process.env.REACT_URL,
         methods: ["GET", "POST"],
         transports: ['websocket', 'polling'],
+        allowedHeaders: ["my-custom-header"],
         credentials: true
     }
 });
 
-// // Add headers before the routes are defined
-// app.use(function (req, res, next) {
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
 
-//     // Website you wish to allow to connect
-//     res.setHeader('Access-Control-Allow-Origin', process.env.REACT_URL);
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', process.env.REACT_URL);
 
-//     // Request methods you wish to allow
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-//     // Request headers you wish to allow
-//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-//     // Set to true if you need the website to include cookies in the requests sent
-//     // to the API (e.g. in case you use sessions)
-//     res.setHeader('Access-Control-Allow-Credentials', true);
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
 
-//     // Pass to next layer of middleware
-//     next();
-// });
+    // Pass to next layer of middleware
+    next();
+});
 
-// Cors khi p
-const corsOptions = {
-    origin: process.env.REACT_URL,
-    methods: 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
-    credentials: true, //access-control-allow-credentials:true
-    optionSuccessStatus: 200,
-};
+// // Cors khi p
+// const corsOptions = {
+//     origin: process.env.REACT_URL,
+//     methods: 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+//     credentials: true, //access-control-allow-credentials:true
+//     optionSuccessStatus: 200,
+// };
 
-// cho phép kết nối với địa chỉ website khác
-app.use(cors(corsOptions));
+// // cho phép kết nối với địa chỉ website khác
+// app.use(cors(corsOptions));
 
 //user middleware
 app.use(helmet())
